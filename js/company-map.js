66902d8a01c302c95dd8db957450ea2124282f36
ondajung <rhok13@naver.com>
2026-07-24 14:55:30 +0900

    Update company-map.js

--- js/company-map.js
+++ js/company-map.js
@@ -16,7 +16,7 @@ leafletJS.onload = function() {
 document.head.appendChild(leafletJS);
 
 function initializeCompanyMap() {
-    // Correct Company location coordinates (37°19'25.5"N 127°13'11.4"E)
+    // Exact coordinates for 37°19'25.5"N 127°13'11.4"E
     const companyLat = 37.323750;
     const companyLng = 127.219833;
     
@@ -33,12 +33,11 @@ function initializeCompanyMap() {
     const goldIcon = L.icon({
         iconUrl: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%23C5A059"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>',
         iconSize: [36, 36],
-        iconAnchor: [18, 36],
-        popupAnchor: [0, -36]
+        iconAnchor: [18, 36]
     });
     
-    // Add marker at exact coordinates (Without automatic popup)
-    const marker = L.marker([companyLat, companyLng], { icon: goldIcon }).addTo(map);
+    // Add marker at exact coordinates (Without popup card)
+    L.marker([companyLat, companyLng], { icon: goldIcon }).addTo(map);
     
     // Position zoom controls
     map.zoomControl.setPosition('topright');
